20347dece840984c1b71ccbbe6f4d98efeb3d146
Prajjwal-dev <prajjwal.maharjan2013@gmail.com>
2024-03-01 09:48:12 +0545

    New Commit

---
+++ string2.py
@@ -0,0 +1,7 @@
+name = "Superman"
+print(name.lower())
+print(name.upper())
+print(name.find("m"))
+print(name.replace("H","S"))
+print(name)
+print("Superman" in name)
---
+++ type conversion.py
@@ -0,0 +1,4 @@
+first = input("Enter the 1st number: ")
+second = input("Enter the 2nd number: ")
+sum = int(first) + int(second)
+print(sum)
